f087cf2d39ccfb48ad5d5d4f91cb157c32c9b2b5
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-02-07 15:43:38 +0700

    fix: error connect to mongodb

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,14 +1,25 @@
-import mongoose from 'mongoose'
+import mongoose, { ConnectOptions } from 'mongoose'
 import config from '../config/environment'
 import { logger } from './logger'
 
+// mongoose
+//   .connect(`${config.db}`)
+//   .then(() => {
+//     logger.info('Connected to MongoDB')
+//   })
+//   .catch((eror) => {
+//     logger.info('Failed to connect to MongoDB')
+//     logger.error(eror)
+//     process.exit(1)
+//   })
+
 mongoose
-  .connect(`${config.db}`)
+  .connect(`${config.db}`, { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions)
   .then(() => {
     logger.info('Connected to MongoDB')
   })
-  .catch((eror) => {
-    logger.info('Failed to connect to MongoDB')
-    logger.error(eror)
+  .catch((error) => {
+    logger.error('Failed to connect to MongoDB')
+    logger.error(error)
     process.exit(1)
   })
